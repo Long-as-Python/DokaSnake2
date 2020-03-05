ca27411cc0c89d792cfbc277087749ab6e3ce0d4
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2020-03-05 19:03:02 +0200

    init

---
+++ DokaSnake/DokaSnake/DokaSnake.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+const int HEIGHT = 10;
+const int WIDTH = 10;
+
+int map[HEIGHT][WIDTH]; //0 - empty, 1 - wall, 2 - food
+
+void MapBuilder() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; i < WIDTH; j++) {
+            //if
+        }
+    }
+}
+
+int main()
+{
+    return 0;
+}
